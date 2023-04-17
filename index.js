946044befe197bff5aef95281f0003c80e7926f7
Won Rhim <wwrk22@gmail.com>
2023-04-17 16:07:29 -0400

    Add arithmetic functions

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+let operandA;
+let operandB;
+let operator;
+let result;
+
+
+function operate(a, b, op) {
+  switch (op) {
+    case 'add':
+      return add(a, b);
+    case 'subtract':
+      return subtract(a, b);
+    case 'multiply':
+      return multiply(a, b);
+    case 'divide':
+      return divide(a, b);
+    default:
+      return "ERROR";
+  }
+}
