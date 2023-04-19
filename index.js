5d300f0930c8ca9889368e9e68acbc63b15350b5
Won Rhim <wwrk22@gmail.com>
2023-04-18 20:06:41 -0400

    Improve and refactor digit append

--- index.js
+++ index.js
@@ -38,26 +38,40 @@ function operate(a, b, op) {
 /* Number button event listener */
 const numberButtonListener = function(button) {
   button.addEventListener("click", () => {
-    registerNumber(button.dataset.key);
+    registerDigit(button.dataset.key);
   });
 };
 
-// If we have an operator to execute, then start forming the second operand.
+// If we have an operator to execute, then append digit to the second operand.
 // Otherwise, append number to the first operand.
-const registerNumber = function(number) {
+const registerDigit = function(digit) {
   const output = document.querySelector('#output');
 
   if (operator === "") {
-    firstOperand += number;
+    firstOperand = appendDigit(firstOperand, digit);
     output.textContent = firstOperand;
-  } else { // we're on the second operand
-    secondOperand += number;
+  } else {
+    secondOperand = appendDigit(secondOperand, digit);
     output.textContent = secondOperand;
   }
 };
 
+function appendDigit(operand, digit) {
+  if (digit === ".") {
+    if (operand.includes(".") === false) {
+      operand += digit;
+    }
+  } else if (digit === "0") {
+    if (operand !== "0") {
+      operand += digit;
+    }
+  } else {
+    operand += digit;
+  }
+
+  return operand
+}
+
 
 const numberButtons = document.querySelectorAll('.number');
-numberButtons.forEach(numberButtonListener)
-
-
+numberButtons.forEach(numberButtonListener);
